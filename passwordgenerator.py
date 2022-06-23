35271db16f451f770690a71f460b6476e3afa9c7
bbbryan8910 <98760105+bbbryan8910@users.noreply.github.com>
2022-06-22 21:11:19 -0500

    Update passwordgenerator.py
    Gave the option to choose how long you want the password to be

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -4,10 +4,11 @@ lower_case = "abcdefghijklmnopqrstuvwxyz"
 upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 number = "0123456789"
 symbols = "!@#$%^&*()_+{}][\/.,;:"
-
 Use_for = lower_case + upper_case + number + symbols
+usefor = str(Use_for)
 #Length for pass is how long you want the password to be 
-length_for_pass = 18
-
-password = "".join(random.sample(Use_for, length_for_pass))
-print("Your Randomly Generated Password Is:" + password)
+length_for_pass = input("How long do you want the password to be? (Be sure to incude only numbers and stay within the 84 character limit) ")
+lengthforpass = int(length_for_pass)
+password = "".join(random.sample(usefor, lengthforpass))
+passy = str(password)
+print("Your Randomly Generated Password Is :" + passy)
